10c73da4341aa77028ad8b0d5da781c8729ff3e1
Elmer Ivan Reales Flores <20220240@ricaldone.edu.sv>
2024-05-14 21:32:58 -0600

    ejercicio 2

--- ejercicio2/App.js
+++ ejercicio2/App.js
@@ -10,62 +10,66 @@ import {
   Modal,
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-
+ 
 const App = () => {
   // Estados para el nombre del cliente, fecha de reserva, lista de clientes, y visibilidad del modal
   const [nombre, setNombre] = useState('');
   const [fechaReserva, setFechaReserva] = useState(new Date());
-  const [clientes, setClientes] = useState([]);
+  const [personaReserva, setPersonaReserva] = useState('');
+  const [alumno, setAlumnos] = useState([]);
+  const [carnet, setCarnet] = useState([]);
+  const [colorFav, setColorfav] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
-
+ 
   // Estados para el datetimepicker
   const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState('date');
   const [show, setShow] = useState(false);
-
+ 
   // Función para cambiar la fecha seleccionada en el datetimepicker
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date; // Si no se selecciona ninguna fecha, se mantiene la actual
     setShow(false); // Oculta el datetimepicker
     setFechaReserva(currentDate); // Establece la fecha de reserva seleccionada en el estado
   };
-
+ 
   // Función para mostrar el datetimepicker con el modo especificado (date o time)
   const showMode = (currentMode) => {
     setShow(true); // Muestra el datetimepicker
     setMode(currentMode); // Establece el modo del datetimepicker
   };
-
+ 
   // Función para mostrar el datetimepicker en modo fecha
   const showDatepicker = () => {
     showMode('date');
   };
-  const [idCounter, setIdCounter] = useState(1);
+ 
   // Función para agregar un nuevo cliente
-  const agregarCliente = () => {
+  const agregarAlumno = () => {
     // Genera un nuevo cliente con un ID único (incrementa el último ID generado)
-    const nuevoCliente = { id: idCounter, nombre: nombre, fechaReserva: fechaReserva };
+    const nuevoAlumno = { id: alumno.length + 1, nombre: nombre, carnet: carnet, colorFav: colorFav, fechaReserva: fechaReserva, persoReserva: personaReserva };
     // Agrega el nuevo cliente a la lista de clientes
-    setClientes([...clientes, nuevoCliente]);
-    // Incrementa el contador de ID
-    setIdCounter(idCounter + 1);
+    setAlumnos([...alumno, nuevoAlumno]);
     // Limpia los campos de entrada
     setNombre('');
+    setCarnet('');
+    setColorfav('');
     setFechaReserva(new Date());
+    setPersonaReserva('');
     // Oculta el modal de agregar cliente
     setModalVisible(false);
   };
-
+ 
   // Función para eliminar un cliente
-  const eliminarCliente = (id) => {
+  const eliminarAlumno = (id) => {
     // Filtra la lista de clientes para excluir el cliente con el ID dado
-    setClientes(clientes.filter((cliente) => cliente.id !== id));
+    setAlumnos(alumno.filter((alumno) => alumno.id !== id));
   };
-
+ 
   return (
     <View style={styles.container}>
       {/* Botón para abrir el modal de agregar cliente */}
-      <Button title="Agregar Cliente" onPress={() => setModalVisible(true)} />
+      <Button title="Agregar Alumno" onPress={() => setModalVisible(true)} />
       {/* Modal de agregar cliente */}
       <Modal
         animationType="slide"
@@ -80,12 +84,25 @@ const App = () => {
             {/* Campo de entrada para el nombre del cliente */}
             <TextInput
               style={styles.input}
-              placeholder="Nombre del Cliente"
+              placeholder="Nombre"
               value={nombre}
               onChangeText={setNombre}
             />
+            <TextInput
+              style={styles.input}
+              placeholder="Carnet"
+              keyboardType='numeric'
+              value={carnet}
+              onChangeText={setCarnet}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Materia favorita"
+              value={colorFav}
+              onChangeText={setColorfav}
+            />
             {/* Botón para mostrar el datetimepicker */}
-            <TouchableOpacity onPress={showDatepicker}><Text>Seleccionar fecha de Reserva</Text></TouchableOpacity>
+            <TouchableOpacity onPress={showDatepicker}><Text>Seleccionar fecha de nacimiento</Text></TouchableOpacity>
             {/* Muestra la fecha seleccionada */}
             <Text>selected: {fechaReserva.toLocaleString()}</Text>
             {/* Muestra el datetimepicker si la variable show es verdadera */}
@@ -100,7 +117,7 @@ const App = () => {
               />
             )}
             {/* Botón para agregar el cliente */}
-            <Button title="Agregar Cliente" onPress={agregarCliente} />
+            <Button title="Agregar Alumno" onPress={agregarAlumno} />
             {/* Botón para cancelar y cerrar el modal */}
             <Button
               title="Cancelar"
@@ -112,32 +129,40 @@ const App = () => {
       </Modal>
       {/* Lista de clientes */}
       <FlatList
-        data={clientes}
+        data={alumno}
         renderItem={({ item }) => (
-          
           <TouchableOpacity
             style={styles.clienteItem}
           >
-            
+ 
             {/* Muestra el ID, nombre y fecha de reserva del cliente */}
-            <Text style={styles.clienteNombre}>{item.id}</Text>
+ 
+            <View style={styles.conte}>
+              <Text style={styles.clienteNombre}>{item.id}</Text>
+              <TouchableOpacity
+              style={styles.buttonEliminar}
+                onPress={() => eliminarAlumno(item.id)}>
+                <Text style={styles.clienteNombre}>X</Text>
+              </TouchableOpacity>
+            </View>
+ 
             <Text style={styles.clienteNombre}>{item.nombre}</Text>
+            <Text style={styles.clienteNombre}>{item.carnet}</Text>
+            <Text style={styles.clienteNombre}>{item.colorFav}</Text>
             <Text style={styles.clienteFecha}>
-              Fecha de Reserva: {item.fechaReserva.toDateString()}
+              Fecha de nacimiento: {item.fechaReserva.toDateString()}
             </Text>
-            <Button
-              title="Eliminar Cliente"
-              onPress={() => eliminarCliente(item.id)}
-              color="red"
-            />
+ 
+ 
           </TouchableOpacity>
+ 
         )}
         keyExtractor={(item) => item.id.toString()} // Extrae el ID de cada cliente como clave única
       />
     </View>
   );
 };
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -168,7 +193,7 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
     borderRadius: 5,
-    marginTop:5
+    marginTop: 5
   },
   clienteNombre: {
     fontSize: 18,
@@ -177,6 +202,12 @@ const styles = StyleSheet.create({
   clienteFecha: {
     fontSize: 16,
   },
+  conte: {
+    flexDirection: 'row',
+  },
+  buttonEliminar: {
+    marginLeft: 300,
+  },
 });
-
+ 
 export default App;
